27d75f0bc4e3b0a6b52782dc6cdc3352405f0eec
Antoine07 <antoine.lucsko@wanadoo.fr>
2026-03-06 10:16:12 +0100

    drizzle

---
+++ starter-drizzle/src/server.ts
@@ -0,0 +1,36 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { asc, eq } from "drizzle-orm";
+import { movies, screenings, rooms } from "./drizzle/schema";
+import { pool } from "./db"
+
+export const db = drizzle(pool);
+
+export async function listMovies() {
+  const items = await db
+    .select()
+    .from(movies)
+    .orderBy(asc(movies.id));
+
+  return items;
+}
+
+listMovies().then(console.log)
+
+
+export async function getMovieSchedule(movieId: string) {
+  return db
+    .select({
+      movieId: movies.id,
+      movieTitle: movies.title,
+      screeningId: screenings.id,
+      startTime: screenings.startTime,
+      roomName: rooms.name,
+    })
+    .from(movies)
+    .innerJoin(screenings, eq(screenings.movieId, movies.id))
+    .innerJoin(rooms, eq(rooms.id, screenings.roomId))
+    .where(eq(movies.id, movieId))
+    .orderBy(asc(screenings.startTime));
+}
+
+getMovieSchedule("8c2d4f91-1a6b-4c3e-8f57-9d2a6b4e1c22").then(console.log)
